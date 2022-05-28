67031ae450a83d6516e6538bc3b142b78dad1b9f
MarvinLin <atimis19@gmail.com>
2022-05-28 22:53:13 +0800

    add: project init

---
+++ BackwardsClock/Shared/BackwardsClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackwardsClockApp.swift
+//  Shared
+//
+//  Created by cm0679 on 2022/5/28.
+//
+
+import SwiftUI
+
+@main
+struct BackwardsClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BackwardsClock/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by cm0679 on 2022/5/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
