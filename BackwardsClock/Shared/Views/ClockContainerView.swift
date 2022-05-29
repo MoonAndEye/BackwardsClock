0a4bfd1f8c94e898d12ab73029fdc616b2287b6f
MarvinLin <atimis19@gmail.com>
2022-05-29 10:52:51 +0800

    add: secondHand view

---
+++ BackwardsClock/Shared/Views/ClockContainerView.swift
@@ -0,0 +1,20 @@
+//
+//  ClockContainerView.swift
+//  BackwardsClock
+//
+//  Created by cm0679 on 2022/5/28.
+//
+
+import SwiftUI
+
+struct ClockContainerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ClockContainerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ClockContainerView()
+    }
+}
---
+++ BackwardsClock/Shared/Views/SecondHandView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondHandView.swift
+//  BackwardsClock
+//
+//  Created by cm0679 on 2022/5/29.
+//
+
+import SwiftUI
+
+struct SecondHandView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondHandView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondHandView()
+    }
+}
