172671503dbf3d8716dd24d3f00762a68d68159f
MarvinLin <atimis19@gmail.com>
2022-06-05 22:32:37 +0800

    feat(add): after some time, change main view

--- BackwardsClock/Shared/Views/LaunchQuote/LaunchQuoteView.swift
+++ BackwardsClock/Shared/Views/LaunchQuote/LaunchQuoteView.swift
@@ -8,8 +8,19 @@
 import SwiftUI
 
 struct LaunchQuoteView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        QuoteMessageView()
+            .onAppear {
+                switchToMainView()
+            }
+    }
+    
+    private func switchToMainView() {
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            Router.shared.currentState = .backwardsClock
+        }
     }
 }
 
