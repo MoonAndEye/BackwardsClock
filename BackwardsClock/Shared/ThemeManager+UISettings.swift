71eb813f0f4cf4343e532436bf495d897a3d90d1
MarvinLin <atimis19@gmail.com>
2022-07-02 20:29:44 +0800

    add: Theme manager with color

---
+++ BackwardsClock/Shared/ThemeManager+UISettings.swift
@@ -0,0 +1,28 @@
+//
+//  ThemeManager+UISetting.swift
+//  BackwardsClock
+//
+//  Created by cm0679 on 2022/7/2.
+//
+
+import SwiftUI
+// MARK: - 預設值
+extension ThemeManager {
+    
+    static var defaultSecondHandColor: Color {
+        Color(red: 255 / 255, green: 59 / 255, blue: 48 / 255, opacity: 1.0)
+    }
+    
+    static var defaultMinuteHandColor: Color {
+        return .black
+    }
+    
+    static var defaultHourHandColor: Color {
+        return .green
+    }
+}
+
+extension ThemeManager {
+    
+    
+}
