3a819a4e0473901f09da9f0f779511f87ee00ad3
MarvinLin <atimis19@gmail.com>
2022-07-05 08:33:48 +0800

    add: Crashlytics and Analytics Manager

---
+++ BackwardsClock/Shared/AnalyticsManager.swift
@@ -0,0 +1,13 @@
+//
+//  AnalyticsManager.swift
+//  BackwardsClock (iOS)
+//
+//  Created by cm0679 on 2022/7/5.
+//
+
+import Foundation
+import FirebaseAnalytics
+// 文件寫，這並不適用 macOS
+#if os(iOS)
+
+#endif
